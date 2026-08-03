d153c858980e0156592d888b48e02cb61e881c08
mttaybii-art <mt.taybii@gmail.com>
2026-08-03 05:42:50 -0700

    Update Todo list.react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import "./App.css";
 import Header from "./components/Header";
+import {Todos} from "./components/Todos";
+import {TodoItems} from "./components/TodoItems";
+import {Footer} from "./components/Footer"; 
 function App() {
   return (
     <>
       <Header/>
+      <Todos/>
+      <TodoItems/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/TodoItems.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const TodoItems = () => {
+  return (
+    <div>TodoItems</div>
+  )
+}
+
